1a44c122f2c90677570a103b34ab61be67bda08e
sacharora999 <sacharora999@gmail.com>
2023-05-01 21:34:28 +0530

    Initial Commit

---
+++ localvar.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract mylocalcon
+{
+    function myfunc() pure public returns(uint){
+        uint age = 20;
+        return age;
+
+    }
+
+}
