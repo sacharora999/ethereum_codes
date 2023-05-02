44274f8e32994801713ac4f5e2c075ad21d046ef
sacharora999 <sacharora999@gmail.com>
2023-05-02 11:20:08 +0530

    Setters and Getters

---
+++ getsandset.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract getandset{
+
+    uint age = 20;
+    function getter() public view returns (uint){
+        return age;
+    }
+
+    function setter(uint new_age) public{
+        age = new_age;
+    }
+}
