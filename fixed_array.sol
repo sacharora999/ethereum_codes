b2d007cb0338efe8baf42b6c2978606f1334b706
sacharora999 <sacharora999@gmail.com>
2023-05-03 06:21:38 +0530

    Fixed Size Array in Solidity

---
+++ fixed_array.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract myarraycon{
+
+    uint [4] public myarr = [10,20,30,40]; 
+    function setter(uint ind, uint val) public 
+    {
+          myarr[ind] = val;  
+    } 
+
+    function get_len() public view returns(uint)
+    {
+        return myarr.length;
+    }
+
+}
